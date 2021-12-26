205c48cb3ac314565b065697106f9218765af995
KuznetsovM <kuznetcoff@mail.ru>
2021-12-26 19:02:16 +0300

    init commit

---
+++ src/main/java/ru/gb/gbcartcreator/GbCartCreatorApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gbcartcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbCartCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbCartCreatorApplication.class, args);
+    }
+
+}
